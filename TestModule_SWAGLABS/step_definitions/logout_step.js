75ab26688eb082f07ad7a78ea3d97c29217689fd
pappani <57202968+prakashappani@users.noreply.github.com>
2020-02-12 22:30:36 -0800

    Update logout_step.js

--- TestModule_SWAGLABS/step_definitions/logout_step.js
+++ TestModule_SWAGLABS/step_definitions/logout_step.js
@@ -3,7 +3,7 @@ module.exports = function () {
 	this.When(/^I logout from the application$/, function (callback) {
 		logoutPage.clickhamburger().then(function () {
 			logoutPage.logOutButton().click().then(function () {
-				browser.sleep(2000).then(function () {
+				browser.sleep(1000).then(function () {
 					callback();
 				});
 			});
