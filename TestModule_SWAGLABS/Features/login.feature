38ebf79c1299671b4fe543674dd2f20f0a42e222
Prakash <papapni@gmail.com>
2020-02-25 20:57:50 -0800

    used data table feature

--- TestModule_SWAGLABS/Features/login.feature
+++ TestModule_SWAGLABS/Features/login.feature
@@ -1,21 +1,34 @@
-Feature: UIAssessment
+Feature: Automation of SWAG LABS
 
   Scenario: Verify user is able to login
     Given I load the url
     When I enter valid credentials
     Then I should see the Inventory container
 
-  Scenario Outline: Verify user is able to add items to the cart
+  Scenario Outline: Verify user is able to add  ONE items to the cart using Examples
     Given I'm on the Inventory container
     When I Add the <Item1> to the cart
-    And I Add the <Item2> to the cart
     And I click on the shopping cart button
-    Then I should be able to see the items <Item1> and <Item2> in the cart
+    Then I should be able to see the items <Item1>  in the cart
+    And I click on continue shopping button
     Examples:
-      | Item1             | Item2                 |
-      | Sauce Labs Onesie | Sauce Labs Bike Light |
+      | Item1             |
+      | Sauce Labs Onesie |
 
-  Scenario: Verify AllSites User is able to Logout from the application
+  Scenario: Verify user is able to add multiple items to the cart using Data Table
+    Given I'm on the Inventory container
+    And I add "More Products" to the shopping cart
+      | Products                 |
+      | Sauce Labs Bike Light    |
+      | Sauce Labs Fleece Jacket |
+    And I click on the shopping cart button
+    Then I should verify "All Products" are in the shopping cart
+      | Products                 |
+      | Sauce Labs Onesie        |
+      | Sauce Labs Bike Light    |
+      | Sauce Labs Fleece Jacket |
+
+  Scenario: Verify User is able to Logout from the application
     Given I'm on the Inventory container
     When I logout from the application
     Then I should go back to the login page
--- TestModule_SWAGLABS/step_definitions/shopping-step.js
+++ TestModule_SWAGLABS/step_definitions/shopping-step.js
@@ -1,10 +1,9 @@
-
-module.exports = function() {
+module.exports = function () {
     'use strict';
     var i;
 
     this.Given(/^I'm on the Inventory container$/, function (callback) {
-        ShoppingPage.getcartList().isDisplayed().then(function(isPresent) {
+        ShoppingPage.getcartList().isDisplayed().then(function (isPresent) {
             expect(isPresent).to.equal(true);
             callback();
         });
@@ -12,10 +11,10 @@ module.exports = function() {
 
 
     this.When(/^I Add the (.*) to the cart$/, function (item1, callback) {
-        if(item1 =='Sauce Labs Bike Light' )
-            i =1;
+        if (item1 == 'Sauce Labs Bike Light')
+            i = 1;
         else
-            i=4;
+            i = 4;
         ShoppingPage.getitemsList().get(i).click().then(function () {
             callback();
         });
@@ -23,7 +22,7 @@ module.exports = function() {
 
 
     this.When(/^I click on the shopping cart button$/, function (callback) {
-        element(by.css('.shopping_cart_container')).click().then(function(){
+        element(by.css('.shopping_cart_container')).click().then(function () {
             browser.sleep(1000).then(function () {
                 callback();
             });
@@ -31,31 +30,57 @@ module.exports = function() {
 
     });
 
-    this.Then(/^I should be able to see the items (.*) and (.*) in the cart$/, function (item1, item2, callback) {
+    this.Then(/^I should be able to see the items (.*) in the cart$/, function (item1, callback) {
         var list = element.all(by.css('.inventory_item_name'));
-        list.get(0).getText().then(function(text) {
-            list.get(1).getText().then(function (text2) {
-                expect(text).to.equal(item1);
-                expect(text2).to.equal(item2);
-                callback();
-            });
+        list.get(0).getText().then(function (text) {
+            expect(text).to.equal(item1.trim());
+            callback();
         });
     });
 
 
     this.Then(/^I should see the Inventory container$/, function (callback) {
-        element(by.css('.app_logo')).isDisplayed().then(function(isPresent) {
+        element(by.css('.app_logo')).isDisplayed().then(function (isPresent) {
             expect(isPresent).to.equal(true);
             callback();
         });
     });
+
+
+    this.When(/^I add (.*) to the shopping cart$/, function (items, table, callback) {
+        for (let i = 0; i < table.hashes().length; i++) {
+            let name = table.rows()[i].toString()
+            let xpth = `//div[contains(text(), '${name}')]`
+            element(by.xpath(xpth)).click()
+            element(by.xpath('//button[@class=\'btn_primary btn_inventory\']')).click()
+            element(by.xpath('//button[@class=\'inventory_details_back_button\']')).click()
+            callback()
+        }
+    });
+
+
+    this.Then(/^I should verify "All Products" are in the shopping cart$/, function (table, callback) {
+        var list = element.all(by.css('.inventory_item_name'));
+        for (let i = 0; i < table.hashes().length; i++) {
+            let exp = table.rows()[i].toString()
+            list.get(i).getText().then(function (text) {
+                expect(text).to.equal(exp);
+            });
+        }
+        callback();
+    });
+
+    this.Then(/^I click on continue shopping button$/, function (callback) {
+        // element(by.xpath('//a[@class=\'btn_secondary\']')).click()
+
+        element(by.xpath('//a[@class=\'btn_secondary\']')).click().then(function () {
+            browser.sleep(1000).then(function () {
+                callback();
+            });
+        });
+    });
+
 };
-
-
-
-
-
-
 
 
 
